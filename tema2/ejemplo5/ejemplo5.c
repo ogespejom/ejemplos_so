1d3d0f7fe39b8f065fa0933be098519e8888f88e
ogespejom <ogespejom@udistrital.edu.co>
2026-06-24 00:27:41 -0500

    Adicion del ejemplo 5

---
+++ tema2/ejemplo5/ejemplo5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+long contador;
+
+void* cuenta(void*){
+	while(1) contador++;
+}
+
+int main(){
+	pthread_t id_h;
+	int ret_h;
+	void *arg=NULL;
+
+	contador = 0;
+
+	ret_h = pthread_create(&id_h, NULL, &cuenta, arg);
+	if(ret_h){
+		printf("ERROR");
+		exit(1);
+	}
+	printf("El hilo se encuentra funcionando en segundo plano...");
+	getchar();
+	printf("El contador llegó hasta: %ld\n", contador);
+	exit(0);	
+}
